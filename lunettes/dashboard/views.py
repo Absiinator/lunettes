0b9382f4395cc51727778d57ecf46550afa4480d
Absiinator <jeffreylepage51@gmail.com>
2022-04-28 23:37:45 +0200

    added core df for distribution of genders in dashboard

--- lunettes/dashboard/views.py
+++ lunettes/dashboard/views.py
@@ -1,9 +1,25 @@
+from calendar import c
+from re import I
 from django.shortcuts import render
 from .decorators import *
 from django.contrib.auth.decorators import login_required
+import pandas as pd
+
+from costumer.models import *
+from products.models import *
 
 # Create your views here.
 @login_required(login_url='authentification')
 def dashboard(request):
-    context = {'pageActive': 'dashboard'}
+    products = Product.objects.all()
+    costumers = Costumer.objects.all()
+
+    # create a dataframe of gender distribution
+    dist_df = pd.DataFrame({'Male': [costumers.filter(gender__name='Male').count()], 
+                        'Female': [costumers.filter(gender__name='Female').count()]})
+
+
+    context = {'pageActive': 'Dashboard',
+                'products': products,
+                'distribution': dist_df}
     return render(request, 'dashboard/dashboard.html', context)
